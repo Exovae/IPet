348ab756a4e0519e30300954c193a371ae426984
Exovae <elliot@keymetricsoft.com>
2023-02-27 21:17:22 -0500

    Add project files.

---
+++ IPet/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+interface IPet
+{
+    string Name { get; set; }
+    int Age { get; set; }
+    string MakeSound();
+}
+
+class Dog : IPet
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public string MakeSound()
+    {
+        return "Woof!";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog myDog = new Dog();
+        myDog.Name = "Fido";
+        myDog.Age = 3;
+
+        Console.WriteLine($"My dog {myDog.Name} is {myDog.Age} years old.");
+        Console.WriteLine($"He says '{myDog.MakeSound()}'");
+
+        Console.ReadLine();
+    }
+}
